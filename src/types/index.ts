c8d11289f78d21a418b5fc581171bef07b48e3f1
下山化缘的小和尚 <72691225+workcjl@users.noreply.github.com>
2021-06-19 21:03:10 +0800

    feat: 定义类型约束文件

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+/** config中method选项的类型约束 */
+export type Method =
+  | 'get'
+  | 'delete'
+  | 'head'
+  | 'options'
+  | 'post'
+  | 'put'
+  | 'patch'
+  | 'GET'
+  | 'DELETE'
+  | 'HEAD'
+  | 'OPTIONS'
+  | 'POST'
+  | 'PUT'
+  | 'PATCH'
+
+/** config的类型约束 */
+export interface AxiosRequestConfig {
+  url: string /** url是必须要传递的参数 */
+  method?: Method /** method可选，因为默认会给‘get' */
+  data?: any /** data可选 */
+  params?: any /** params可选 */
+}
